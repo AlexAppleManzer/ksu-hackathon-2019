90c4623d835000b31c126a7bfdc037c8726ef275
LionHeart <btedesch@students.kennesaw.edu>
2019-09-27 15:08:32 -0400

    Please work this time...

--- frontend/node_modules/angular-6-social-login/entities/login-provider.d.ts
+++
@@ -1,6 +0,0 @@
-import { SocialUser } from './user';
-export interface LoginProvider {
-    initialize(): Promise<SocialUser>;
-    signIn(): Promise<SocialUser>;
-    signOut(): Promise<any>;
-}
--- frontend/node_modules/angular-6-social-login/index.d.ts
+++
@@ -1,4 +0,0 @@
-export { SocialLoginModule } from './auth.module';
-export { AuthService, AuthServiceConfig } from './auth.service';
-export { LoginProvider, SocialUser, LoginProviderClass, LinkedInResponse } from './entities/index';
-export { FacebookLoginProvider, GoogleLoginProvider, LinkedinLoginProvider } from './providers/index';
